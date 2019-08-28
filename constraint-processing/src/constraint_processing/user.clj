aa49da77345548ead46451d15e4007236b634ada
Phillip van Heerden <vanheerden.phillip@gmail.com>
2019-08-28 14:56:03 +0200

    Added with-parse-fn macro for ergonomic rebinding of *parse-fn*

--- constraint-processing/src/constraint_processing/user.clj
+++ constraint-processing/src/constraint_processing/user.clj
@@ -13,25 +13,32 @@
            TacasParser
            PaperExample))
 
+(defmacro with-parse-fn
+  "Rebind `constraint-processing.table/*parse-fn*` to `parse-fn`, and
+  then evaluate `body` inside of the binding. Also wraps the form in
+  a Tufte profile form."
+  [parse-fn & body]
+  `(tufte/profile {} (binding [table/*parse-fn* ~parse-fn]
+      ~@body)))
+
 ;; TACAS -- works well! learning stalls at depth 3, finds evidence at depth 4
-(tufte/profile {} (binding [table/*parse-fn* #(TacasParser/parse %)]
-   (let [table (learner/learn 3)
-         learnt (sfa/table->sfa table)]
-     (pprint (meta table))
-     (sfa/sfa->img learnt))))
+(with-parse-fn #(TacasParser/parse %)
+  (let [table (learner/learn 3)
+        learnt (sfa/table->sfa table)]
+    (sfa/sfa->img learnt)))
 
 ;; Paper example -- works well!
-(tufte/profile {} (binding [table/*parse-fn* #(PaperExample/parse %)]
-                    (let [table (learner/learn 4)
-                          learnt (sfa/table->sfa table)]
-                      (pprint table)
-                      (sfa/sfa->img learnt))))
+(with-parse-fn #(PaperExample/parse %)
+  (let [table (learner/learn 4)
+        learnt (sfa/table->sfa table)]
+    (pprint table)
+    (sfa/sfa->img learnt)))
 
 ;; Learn Large -- gets very slow
-(tufte/profile {} (binding [table/*parse-fn* #(LearnLarge/parse %)]
-   (let [table (learner/learn 1)
-         learnt (sfa/table->sfa table)]
-     (pprint table)
-     (sfa/sfa->img learnt))))
+(with-parse-fn #(LearnLarge/parse %)
+  (let [table (learner/learn 1)
+        learnt (sfa/table->sfa table)]
+    (pprint table)
+    (sfa/sfa->img learnt)))
 
 (defonce stats-accumulator (tufte/add-basic-println-handler! {:ns-pattern "*"}))
