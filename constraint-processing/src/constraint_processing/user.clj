0a3498d0fc3c66a85dac5b2041f856078de0e10a
Phillip van Heerden <vanheerden.phillip@gmail.com>
2019-08-29 11:02:47 +0200

    Added an interactive learn function in the user namespace

--- constraint-processing/src/constraint_processing/user.clj
+++ constraint-processing/src/constraint_processing/user.clj
@@ -16,26 +16,46 @@
 (defmacro with-profiling
   "Wrap `body` in a profiling form."
   [& body]
-  `(tufte/profile {} ~@body))
-
-;; TACAS -- works well! learning stalls at depth 3, finds evidence at depth 4
-(with-profiling
-  (let [table (learner/learn {:depth 4, :parse-fn #(TacasParser/parse %)})
-        learnt (table/table->sfa table)]
-    (sfa/sfa->img learnt)))
-
-;; Paper example -- works well!
-(with-profiling
-  (let [table (learner/learn {:depth 4, :parse-fn #(PaperExample/parse %)})
-        learnt (table/table->sfa table)]
-    (pprint table)
-    (sfa/sfa->img learnt)))
-
-;; Learn Large -- gets very slow
-(with-profiling
-  (let [table (learner/learn {:depth 2, :parse-fn #(LearnLarge/parse %)})
-        learnt (table/table->sfa table)]
-    (pprint table)
-    (sfa/sfa->img learnt)))
+  `(taoensso.tufte/profile {} ~@body))
 
 (defonce stats-accumulator (tufte/add-basic-println-handler! {:ns-pattern "*"}))
+
+(defn learn
+  "Learn with `parse-fn` as the oracle, halting when both a fixed-point is
+  reached with the SFAs." ;; TODO Bootstrap the learning process with another table and path db
+  [parse-fn & [{:keys [display?] :or {display? true}}]]
+  (loop [previous-sfa nil
+         depth 1]
+    (let [new-table (learner/learn {:depth depth, :parse-fn parse-fn})
+          new-sfa (table/table->sfa new-table)]
+      (when display
+        (sfa/sfa->img new-sfa))
+      (if (= previous-sfa new-sfa)
+        {:sfa new-sfa, :table new-table, :depth depth}
+        (recur new-sfa (inc depth))))))
+
+(comment
+
+  (learn #(TacasParser/parse %))
+
+  (learn #(TacasParser/parse %) {:display? false})
+
+  ;; TACAS -- works well! learning stalls at depth 3, finds evidence at depth 4
+  (with-profiling
+    (let [table (learner/learn {:depth 3, :parse-fn #(TacasParser/parse %)})
+          learnt (table/table->sfa table)]
+      (sfa/sfa->img learnt)))
+
+  ;; Paper example -- works well!
+  (with-profiling
+    (let [table (learner/learn {:depth 4, :parse-fn #(PaperExample/parse %)})
+          learnt (table/table->sfa table)]
+      (sfa/sfa->img learnt)))
+
+  ;; Learn Large -- gets very slow
+  (with-profiling
+    (let [table (learner/learn {:depth 4, :parse-fn #(LearnLarge/parse %)})
+          learnt (table/table->sfa table)]
+      (sfa/sfa->img learnt)))
+
+ )
