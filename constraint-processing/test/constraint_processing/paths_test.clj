942a2d4b2dabced0b571828fe481f53af974bec6
Phillip van Heerden <vanheerden.phillip@gmail.com>
2019-08-29 09:55:53 +0200

    Added more paths tests

--- constraint-processing/test/constraint_processing/paths_test.clj
+++ constraint-processing/test/constraint_processing/paths_test.clj
@@ -2,14 +2,16 @@
   (:require [constraint-processing.paths :as paths]
             [clojure.test :refer :all]))
 
-
 (deftest single-constraint-path-test
    (let [path [[50 60]]]
      (testing "has one prefix, itself"
        (is (= 1 (count (paths/prefixes path))))
-       (is (= [[50 60]] (first (paths/prefixes path)))))
+       (is (= path (first (paths/prefixes path)))))
      (testing "has one suffix, itself"
        (is (= 1 (count (paths/suffixes path))))
-       (is (= [[50 60]] (first (paths/suffixes path)))))
+       (is (= path (first (paths/suffixes path)))))
      (testing "has a concrete representation of [50]"
-       (is (= [50] (paths/make-concrete path))))))
+       (is (= [50] (paths/make-concrete path)))
+       (is (= [50] (paths/mixed->concrete path))))
+     (testing "has [] as a prefix"
+       (is (= true (paths/prefix? [] path))))))
