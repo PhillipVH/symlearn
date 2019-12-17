3eedc1e842585455eb95dc19502134000686877d
Phillip van Heerden <vanheerden.phillip@gmail.com>
2019-12-17 20:09:58 +0200

    Write results to EDN record

--- src/symlearn/coastal.clj
+++ src/symlearn/coastal.clj
@@ -733,7 +733,7 @@
 (defn -main
    "This function is a collection of forms that test all integrations."
   [& args]
-  (pr (evaluate-benchmark! "regexlib-clean-10.re" 1))
+  (spit "results.edn" (pr-str (evaluate-benchmark! "regexlib-clean-10.re" 1)))
   (stop!)
   (shutdown-agents))
 
