d7f10d7b1588e4c79051fd4dbc579fbd5cea22de
Phillip van Heerden <vanheerden.phillip@gmail.com>
2020-01-09 09:37:11 +0200

    Magic fix for missing SVPABenchmark jar

--- install_symbolicautomata.sh
+++ install_symbolicautomata.sh
@@ -5,3 +5,5 @@ mvn deploy:deploy-file -Dfile=symbolicautomata/SVPABenchmark-0.0.1-SNAPSHOT.jar 
 mvn deploy:deploy-file -Dfile=symbolicautomata/Parsers-1.0.jar -DartifactId=Parsers -Dversion=1.0 -DgroupId=cs.wisc.edu -Dpackaging=jar -Durl=file:lib
 mvn deploy:deploy-file -DgroupId=javacup -DartifactId=cup -Dversion=11 -Dpackaging=jar -Dfile=symbolicautomata/javacup-11c.jar -Durl=file:lib
 
+# Pending investigation
+cp lib/SVPABenchmark/SVPABenchmark/0.0.1-SNAPSHOT/SVPABenchmark-0.0.1-20200109.073211-2.jar lib/SVPABenchmark/SVPABenchmark/0.0.1-SNAPSHOT/SVPABenchmark-0.0.1-20191120.122110-1.jar
