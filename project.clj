ca54c2904d596d23e276086ace8e64ce1eddda1f
Phillip van Heerden <vanheerden.phillip@gmail.com>
2020-03-04 01:21:31 +0200

    Remove old Java files + update Dockerfile

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject symlearn "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Learn Symbolic Finite Automata from Java Parsers using Concolic Execution and Active Learning."
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
@@ -31,7 +31,7 @@
                  [org.apache.commons/commons-lang3 "3.4"]
                  [com.google.guava/guava "18.0"]
                  [javacup/cup "11"]]
-  :main ^:skip-aot symlearn.coastal
+  :main ^:skip-aot symlearn.evaluation
   :repositories {"local" "file:lib"}
   :java-source-paths ["src/main/java"]
   :target-path "target/%s"
